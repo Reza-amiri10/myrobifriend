290cebf0812cdb7a6447c590ef2aa7e49404197c
Apple <mac@Apples-MacBook-Pro.local>
2025-07-10 18:21:42 +0300

    Add new cards to the card list

--- src/App.js
+++ src/App.js
@@ -2,10 +2,35 @@ import Card from "./Card";
 import "tachyons";
 import "./App.css";
 function App() {
+  const roboList = [
+    { name: "Mamy MO", email: "mamymo@gamil.com" },
+    { name: "Dady MO", email: "dadymo@gmail.com" },
+    { name: "Bory MO", email: "borymo@gamil.com" },
+    { name: "Sisy MO", email: "sisymo@gamil.com" },
+  ];
+
   return (
-    <div className="App ">
-      <h1>Hello this a test.</h1>
-      <Card />
+    <div className="App t-row items-center">
+      <Card
+        name={roboList[0].name}
+        email={roboList[0].email}
+        img="https://robohash.org/mamy mo"
+      />
+      <Card
+        name={roboList[1].name}
+        email={roboList[1].email}
+        img="https://robohash.org/dady%20mo"
+      />
+      <Card
+        name={roboList[2].name}
+        email={roboList[2].email}
+        img="https://robohash.org/borom mo"
+      />
+      <Card
+        name={roboList[3].name}
+        email={roboList[3].email}
+        img="https://robohash.org/sisi%20mo"
+      />
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 class Card extends Component {
   render() {
     return (
-      <div className="card-box w-20 br3 b--near-white shadow-3 grow">
-        <img alt="Happy robot" src="https://robohash.org/happy"></img>
-        <h1>Hello it's me</h1>
+      <div className="card-box w-20 br3 b--near-white shadow-3 grow pa3 ma3">
+        <img alt="Happy robot" src={this.props.img}></img>
+        <h1>{this.props.name}</h1>
+        <h2>{this.props.email}</h2>
       </div>
     );
   }
