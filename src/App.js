8f791643791e23b75223885337e42ba7f6a47821
Apple <mac@Apples-MacBook-Pro.local>
2025-07-11 16:25:43 +0300

    Just save the corrent changes to the changes branch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
+import React, { Component } from "react";
 import CardMaker from "./CardMaker";
 import "tachyons";
 import "./App.css";
 import Header from "./Header";
+import roboList from "./RoboList";
 
-function App() {
-  return (
-    <div className="App pa0 ma0">
-      <Header />
-      <main className="mt7 pa3">
-        <div className="lh-copy">
-          <CardMaker />
-        </div>
-      </main>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      roboList: roboList,
+      searchfield: "",
+    };
+  }
+  render() {
+    return (
+      <div className="App pa0 ma0">
+        <Header />
+        <main className="mt7 pa3">
+          <div className="lh-copy">
+            <CardMaker />
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
