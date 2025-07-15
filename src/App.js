7a968f1096b8c3f87003c76f902fa135ec076ad4
Apple <mac@Apples-MacBook-Pro.local>
2025-07-15 11:08:03 +0300

    I add a Scroll component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "tachyons";
 import "./App.css";
 import roboList from "./RoboList";
 import Search from "./Search";
+import Scroll from "./Scroll";
 
 class App extends Component {
   constructor() {
@@ -25,10 +26,18 @@ class App extends Component {
         .includes(this.state.searchvalue.toLowerCase());
     });
     const searchCounts = filterRobots.length;
-    const allCardsLowd = filterRobots.length;
     return (
       <div className="App">
-        <header className=" ma0 pa2 fixed top-0 left-0 right-0 z-999">
+        <div>
+          <h1 className="title mb1 dib f3">MY ROBO</h1>
+          <div className="flex items-center justify-center w-100">
+            <Search ref={this.inputRef} setChange={this.setSearChange} />
+            <h1 className="count f3">
+              {searchCounts > 0 ? `${searchCounts} Matches` : ""}
+            </h1>
+          </div>
+        </div>
+        {/* <header className=" ma0 pa2 fixed top-0 left-0 right-0 z-999">
           <div>
             <h1 className="title mb1 dib f3">MY ROBO</h1>
             <div className="flex items-center justify-center w-100">
@@ -38,8 +47,10 @@ class App extends Component {
               </h1>
             </div>
           </div>
-        </header>
-        {allCardsLowd > 0 ? (
+        </header> */}
+        <Scroll>
+          <CardMaker roboList={filterRobots} />
+          {/* {allCardsLowd > 0 ? (
           <main className="main pa3">
             <div className="lh-copy">
               <hr className="w-100 h-3 black"></hr>
@@ -50,7 +61,8 @@ class App extends Component {
           <main className="main pa3">
             <h1 className="lowding">NO MATCHES</h1>
           </main>
-        )}
+        )} */}
+        </Scroll>
       </div>
     );
   }
